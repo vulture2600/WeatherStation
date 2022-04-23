47d962f71f913ac6e9ff25c648a9eb4188e9deb1
Steve McCluskey <79675737+vulture2600@users.noreply.github.com>
2022-04-23 11:41:59 -0500

    updated getIO.sh and notes.txt

--- html/getIO.sh
+++ html/getIO.sh
@@ -1,19 +1,5 @@
-
 #!/bin/bash
-
-
-#run "sudo chmod +s /bin/raspi-gpio" first for file permissions
-#set pullups for the inputs youre using:
-#"raspi-gpio set XX pu"
-#gets gpio state of pin passed from getDoors.php
-#returns 0 or 1
-
 status=$(raspi-gpio get $1)
-
 value=${status:15:1}
-
 echo $value
-
 exit 0
-
-
